419c9c7a75d9b21df731cdf66f4e32dda0034fcd
blastscout <kennedymr18@gmail.com>
2024-12-16 22:44:21 -0500

    m

---
+++ docs/src/src/contracts/InventoryV1155.sol/contract.InventoryV1155.md
@@ -0,0 +1,159 @@
+# InventoryV1155
+[Git Source](https://github.com//Team3dVidyaGames/InventoryContractV3_erc1155/blob/bdef1cd4d707abe6079f932dd1942af6f315ba5f/src/contracts/InventoryV1155.sol)
+
+**Inherits:**
+AccessControl, ERC1155
+
+
+## State Variables
+### tokenID
+
+```solidity
+uint256 public tokenID = 1;
+```
+
+
+### itemData
+
+```solidity
+mapping(uint256 => Item) private itemData;
+```
+
+
+### itemAttributeInfo
+
+```solidity
+mapping(uint256 => mapping(uint256 => uint256)) public itemAttributeInfo;
+```
+
+
+### ADMIN_ROLE
+
+```solidity
+bytes32 public constant ADMIN_ROLE = keccak256("ADMIN_ROLE");
+```
+
+
+### MINTER_ROLE
+
+```solidity
+bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+```
+
+
+## Functions
+### constructor
+
+
+```solidity
+constructor(string memory baseUri) ERC1155(baseUri);
+```
+
+### addItem
+
+
+```solidity
+function addItem(Item memory newItem) external onlyRole(ADMIN_ROLE);
+```
+
+### updateItemData
+
+
+```solidity
+function updateItemData(Item memory updateItem, uint256 tokenId) external onlyRole(ADMIN_ROLE);
+```
+
+### uri
+
+
+```solidity
+function uri(uint256 tokenId) public view override returns (string memory);
+```
+
+### mint
+
+
+```solidity
+function mint(address to, uint256 tokenId, uint256 amount) external onlyRole(MINTER_ROLE);
+```
+
+### mintBatch
+
+
+```solidity
+function mintBatch(address to, uint256[] memory ids, uint256[] memory values) external onlyRole(MINTER_ROLE);
+```
+
+### burn
+
+
+```solidity
+function burn(address from, uint256 id, uint256 value) external;
+```
+
+### burnBatch
+
+
+```solidity
+function burnBatch(address from, uint256[] memory ids, uint256[] memory values) external;
+```
+
+### fullBalanceOf
+
+
+```solidity
+function fullBalanceOf(address account) external view returns (uint256[] memory);
+```
+
+### tokenExist
+
+
+```solidity
+function tokenExist(uint256 tokenId) public view returns (bool);
+```
+
+### supportsInterface
+
+
+```solidity
+function supportsInterface(bytes4 interfaceId) public view override(AccessControl, ERC1155) returns (bool);
+```
+
+## Events
+### ItemAdded
+
+```solidity
+event ItemAdded(uint256 indexed tokenId, address admin);
+```
+
+### ItemUpdated
+
+```solidity
+event ItemUpdated(uint256 indexed tokenId, address admin);
+```
+
+## Errors
+### ItemDataAndIDMisMatch
+
+```solidity
+error ItemDataAndIDMisMatch(address admin, uint256 length);
+```
+
+### TokenDoesNotExist
+
+```solidity
+error TokenDoesNotExist(uint256 tokenId);
+```
+
+## Structs
+### Item
+
+```solidity
+struct Item {
+    uint256[] attributeData;
+    uint256[] attributeId;
+    string tokenURI;
+    uint256 characterSlot;
+}
+```
+
--- src/contracts/InventoryV1155.sol
+++ src/contracts/InventoryV1155.sol
@@ -4,36 +4,49 @@ pragma solidity ^0.8.6;
 import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 import "@openzeppelin/contracts/access/AccessControl.sol";
 
-contract InventoryV1155 is AccessControl {
-    uint256 public tokenID;
+contract InventoryV1155 is AccessControl, ERC1155 {
+    uint256 public tokenID = 1;
 
-    Item[] private itemData;
-    string[] public slots;
+    mapping(uint256 => Item) private itemData;
+    mapping(uint256 => mapping(uint256 => uint256)) public itemAttributeInfo;
 
     struct Item {
         uint256[] attributeData;
-        uint256 attributeLength;
+        uint256[] attributeId;
         string tokenURI;
         uint256 characterSlot;
     }
 
     //events
     event ItemAdded(uint256 indexed tokenId, address admin);
-
     event ItemUpdated(uint256 indexed tokenId, address admin);
+
+    //errors
+    error ItemDataAndIDMisMatch(address admin, uint256 length);
+    error TokenDoesNotExist(uint256 tokenId);
 
     // Access Control stuff
     bytes32 public constant ADMIN_ROLE = keccak256("ADMIN_ROLE");
     bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
 
     constructor(string memory baseUri) ERC1155(baseUri) {
-        _setupRole(ADMIN_ROLE, msg.sender);
+        _grantRole(ADMIN_ROLE, msg.sender);
         _setRoleAdmin(ADMIN_ROLE, ADMIN_ROLE);
         _setRoleAdmin(MINTER_ROLE, ADMIN_ROLE);
     }
 
     function addItem(Item memory newItem) external onlyRole(ADMIN_ROLE) {
-        itemData.push(newItem);
+        if (newItem.attributeId.length != newItem.attributeData.length) {
+            revert ItemDataAndIDMisMatch(
+                msg.sender,
+                newItem.attributeData.length
+            );
+        }
+        for (uint i = 0; i < newItem.attributeData.length; i++) {
+            itemAttributeInfo[tokenID][newItem.attributeId[i]] = newItem
+                .attributeData[i];
+        }
+        itemData[tokenID] = newItem;
         tokenID++;
         emit ItemAdded(tokenID - 1, msg.sender);
     }
@@ -42,12 +55,36 @@ contract InventoryV1155 is AccessControl {
         Item memory updateItem,
         uint256 tokenId
     ) external onlyRole(ADMIN_ROLE) {
+        if (updateItem.attributeId.length != updateItem.attributeData.length) {
+            revert ItemDataAndIDMisMatch(
+                msg.sender,
+                updateItem.attributeData.length
+            );
+        }
+        if (!tokenExist(tokenId)) {
+            revert TokenDoesNotExist(tokenId);
+        }
+
+        Item storage item = itemData[tokenId];
+
+        //remove Old attributes
+        for (uint i = 0; i < item.attributeData.length; i++) {
+            itemAttributeInfo[tokenID][item.attributeId[i]] = 0;
+        }
+
+        //add new attribute information
+        for (uint i = 0; i < updateItem.attributeData.length; i++) {
+            itemAttributeInfo[tokenID][updateItem.attributeId[i]] = updateItem
+                .attributeData[i];
+        }
+
         itemData[tokenId] = updateItem;
+
         emit ItemUpdated(tokenId, msg.sender);
     }
 
     function uri(uint256 tokenId) public view override returns (string memory) {
-        if (tokenId > tokenID || tokenId == 0) {
+        if (!tokenExist(tokenId)) {
             return super.uri(tokenId);
         } else {
             return itemData[tokenId].tokenURI;
@@ -59,6 +96,9 @@ contract InventoryV1155 is AccessControl {
         uint256 tokenId,
         uint256 amount
     ) external onlyRole(MINTER_ROLE) {
+        if (!tokenExist(tokenId)) {
+            revert TokenDoesNotExist(tokenId);
+        }
         _mint(to, tokenId, amount, "");
     }
 
@@ -67,6 +107,11 @@ contract InventoryV1155 is AccessControl {
         uint256[] memory ids,
         uint256[] memory values
     ) external onlyRole(MINTER_ROLE) {
+        for (uint i = 0; i < ids.length; i++) {
+            if (!tokenExist(ids[i])) {
+                revert TokenDoesNotExist(ids[i]);
+            }
+        }
         _mintBatch(to, ids, values, "");
     }
 
@@ -88,5 +133,29 @@ contract InventoryV1155 is AccessControl {
             revert ERC1155MissingApprovalForAll(sender, from);
         }
         _burnBatch(from, ids, values);
+    }
+
+    function fullBalanceOf(
+        address account
+    ) external view returns (uint256[] memory) {
+        uint256[] memory batchBalances = new uint256[](tokenID);
+
+        for (uint i = 1; i < tokenID; i++) {
+            batchBalances[i] = balanceOf(account, i);
+        }
+
+        return batchBalances;
+    }
+
+    function tokenExist(uint256 tokenId) public view returns (bool) {
+        return (tokenId < tokenID && tokenId != 0);
+    }
+
+    function supportsInterface(
+        bytes4 interfaceId
+    ) public view override(AccessControl, ERC1155) returns (bool) {
+        bool truth = (AccessControl.supportsInterface(interfaceId) ||
+            ERC1155.supportsInterface(interfaceId));
+        return truth;
     }
 }
